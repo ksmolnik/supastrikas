9399c4d19ce97ed48ae0729f1652f7d19e56176c
GGuzinski <102364693+GGuzinski@users.noreply.github.com>
2024-11-12 11:56:40 +0100

    Add files via upload

---
+++ zadanie_php_sql/przychodnia_projekt.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Lis 12, 2024 at 11:55 AM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `przychodnia_projekt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `lekarze`
+--
+
+CREATE TABLE `lekarze` (
+  `id_lekarza` int(11) NOT NULL,
+  `imie` varchar(50) NOT NULL,
+  `nazwisko` varchar(50) NOT NULL,
+  `telefon` varchar(20) NOT NULL,
+  `specjalizacja` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `pacjenci`
+--
+
+CREATE TABLE `pacjenci` (
+  `id_pacjenta` int(11) NOT NULL,
+  `imie` varchar(50) NOT NULL,
+  `nazwisko` varchar(50) NOT NULL,
+  `data_urodzenia` date NOT NULL,
+  `pesel` varchar(11) NOT NULL,
+  `miasto` varchar(50) NOT NULL,
+  `telefon` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `recepta`
+--
+
+CREATE TABLE `recepta` (
+  `id_recepty` int(11) NOT NULL,
+  `nazwa_leku` varchar(100) NOT NULL,
+  `ilosc_leku` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `specjalizacje`
+--
+
+CREATE TABLE `specjalizacje` (
+  `id_specjalizacji` int(11) NOT NULL,
+  `nazwa_specjalizacji` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `wizyty`
+--
+
+CREATE TABLE `wizyty` (
+  `id_wizyty` int(11) NOT NULL,
+  `id_pacjenta` int(11) DEFAULT NULL,
+  `id_lekarza` int(11) DEFAULT NULL,
+  `data` date NOT NULL,
+  `godznia` time NOT NULL,
+  `id_recepty` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `lekarze`
+--
+ALTER TABLE `lekarze`
+  ADD PRIMARY KEY (`id_lekarza`);
+
+--
+-- Indeksy dla tabeli `pacjenci`
+--
+ALTER TABLE `pacjenci`
+  ADD PRIMARY KEY (`id_pacjenta`),
+  ADD UNIQUE KEY `pesel` (`pesel`);
+
+--
+-- Indeksy dla tabeli `recepta`
+--
+ALTER TABLE `recepta`
+  ADD PRIMARY KEY (`id_recepty`);
+
+--
+-- Indeksy dla tabeli `specjalizacje`
+--
+ALTER TABLE `specjalizacje`
+  ADD PRIMARY KEY (`id_specjalizacji`);
+
+--
+-- Indeksy dla tabeli `wizyty`
+--
+ALTER TABLE `wizyty`
+  ADD PRIMARY KEY (`id_wizyty`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `lekarze`
+--
+ALTER TABLE `lekarze`
+  MODIFY `id_lekarza` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pacjenci`
+--
+ALTER TABLE `pacjenci`
+  MODIFY `id_pacjenta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `recepta`
+--
+ALTER TABLE `recepta`
+  MODIFY `id_recepty` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `specjalizacje`
+--
+ALTER TABLE `specjalizacje`
+  MODIFY `id_specjalizacji` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `wizyty`
+--
+ALTER TABLE `wizyty`
+  MODIFY `id_wizyty` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
